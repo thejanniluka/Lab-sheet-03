7f0343591a80ab983bee0050d6b0fa596f69b0f6
Jayawardana JTH <tiran2323@gmail.com>
2020-03-24 00:20:43 +0530

    Configure intent to pass a message, get message and show it

--- app/src/main/java/com/example/labsheet_3/MainActivity.java
+++ app/src/main/java/com/example/labsheet_3/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.labsheet_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view) {
 
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
